8c88d70c8da97a09d7b13449d6fb40447e8a2bc9
Adrian <adrian.she@alumni.ubc.ca>
2015-10-30 14:16:47 -0700

    Add simple HTML table for crime

--- datawranglers/vancrime/views.py
+++ datawranglers/vancrime/views.py
@@ -5,5 +5,8 @@ from .models import Crime
 # Create your views here.
 
 def summary_index(request):
+    all_crimes = Crime.count_all_crimes_by_type()
+    context = {'all_crimes': all_crimes}
+    return render(request, 'vancrime/summary_table.html', context)
 
-def summary_date(request):
+# def summary_date(request):
