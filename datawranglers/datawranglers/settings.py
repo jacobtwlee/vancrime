ee36234dbbb15218a3ff6064ff12dc8e6d4f2c82
Jacob Lee <jacobtwlee@gmail.com>
2015-10-30 00:11:33 -0700

    separate settings for production

--- datawranglers/datawranglers/settings.py
+++ datawranglers/datawranglers/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&*8p5@$3=ta=ag2n4r(3yz9rfskywh(rx#dbc78f2gqxq1(6dy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.203.240.58']
 
 
 # Application definition
@@ -105,5 +105,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
+
+import socket
+
+# Apply dev specific settings if not on production
+if (socket.gethostname() != "VanCrime"):
+    from datawranglers.settings_dev import *
