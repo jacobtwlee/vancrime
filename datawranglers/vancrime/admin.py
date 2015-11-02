0159a5f51a09d62c5cabab0b27777de513a78011
Geoffrey Roeder <geoff.roeder@gmail.com>
2015-11-01 18:57:26 -0800

    registers vancrime with admin backend and customizes displayed fields

--- datawranglers/vancrime/admin.py
+++ datawranglers/vancrime/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from vancrime.models import Crime, Location, LoadedData
+
+class CrimeAdmin( admin.ModelAdmin ):
+    list_display = ('crime_type','location','year','month')
+
+class LocationAdmin( admin.ModelAdmin ):
+    list_display = ('address','latitude','longitude')
+ 
+admin.site.register( Crime, CrimeAdmin )
+admin.site.register( Location, LocationAdmin )
+admin.site.register( LoadedData )
+
+
 
 # Register your models here.
