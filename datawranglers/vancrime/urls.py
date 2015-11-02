b0e339597db7065bafd5cce76575b188cfd77ad1
Adrian <adrian.she@alumni.ubc.ca>
2015-11-02 12:20:26 -0800

    Add month specific summary table

--- datawranglers/vancrime/urls.py
+++ datawranglers/vancrime/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /summary/
-    url(r'summary$', views.summary_index, name = 'summary_table'), 
+    url(r'summary/$', views.summary_index, name = 'summary_table'), 
     # ex: /summary/<year>/<month>
-    # url()
+    url(r'summary/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$', views.summary_date, name = 'summary_specific'),
 ]
