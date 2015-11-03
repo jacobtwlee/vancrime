f61c872a0298d36d97dbf25b229c685468b812df
Adrian <adrian.she@alumni.ubc.ca>
2015-11-02 17:16:34 -0800

    Add method to get summary table

--- datawranglers/vancrime/urls.py
+++ datawranglers/vancrime/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'summary/$', views.summary_index, name = 'summary_table'), 
     # ex: /summary/<year>/<month>
     url(r'summary/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$', views.summary_date, name = 'summary_specific'),
+   # url(r'summary/graph/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$', views.graph, name = 'graph'),
 ]
